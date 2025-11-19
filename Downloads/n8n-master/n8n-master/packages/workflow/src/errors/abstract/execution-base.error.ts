c06d9f1661ffaa7d1ad157b85fcd0601e8e5b5cc
ILYESS24 <ilyessbenina3@gmail.com>
2025-11-19 22:26:45 +0100

    Fix: Replace all remaining @n8n/errors imports with @workflow-automation/errors in workflow errors

--- Downloads/n8n-master/n8n-master/packages/workflow/src/errors/abstract/execution-base.error.ts
+++ Downloads/n8n-master/n8n-master/packages/workflow/src/errors/abstract/execution-base.error.ts
@@ -1,4 +1,4 @@
-import { ApplicationError, type ReportingOptions } from '@n8n/errors';
+import { ApplicationError, type ReportingOptions } from '@workflow-automation/errors';
 
 import type { Functionality, IDataObject, JsonObject } from '../../interfaces';
 
